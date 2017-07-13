6230a5c8795c55fabc1fdc23419ac5a7282d8e3a
= <a.wlezien@gmail.com>
2017-07-12 20:47:47 -0500

    home page done and about page halfway through

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+/*
+    $("#home-nav").on("mouseover",function(){
+
+      console.log("mouseover");
+
+      $(this).parent().css({"bottom":"90%"});
+      $(this).parent().css({"height":"10%"});
+    }).on("mouseout",function(){
+
+      console.log("mouseout");
+
+
+      $(this).parent().css({"height":"130px"});
+      console.log("setting height to 130px");
+      $(this).parent().css({"bottom":"95%"});
+    });
+
+var height = $("#logo").height();
+
+console.log("logo height: " + height);
+$("#logo").css({"width":height+"px"});
+*/
